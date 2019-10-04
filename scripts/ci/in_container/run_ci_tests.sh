f67a8e76a37a77d51d4e14121aec77d3e2a8ae24
Tomek Urbaszek <tomasz.urbaszek@polidea.com>
2019-10-04 18:13:05 +0200

    modules to single tests

--- scripts/ci/in_container/run_ci_tests.sh
+++ scripts/ci/in_container/run_ci_tests.sh
@@ -46,15 +46,6 @@ echo
 echo "Starting the tests with those nose arguments: ${NOSE_ARGS[*]}"
 echo
 set +e
-#nosetests "${NOSE_ARGS[@]}"
-# TODO this is a bad idea but want to check if all tests works
-#find tests/ -type f -not -path '*/\.*' \
-#| grep ".py" \
-#| grep -v ".pyc" \
-#| grep -v "__init__.py" \
-#| grep -v ".*_system.py" \
-#| grep -E "test_.*|.*_test.py" \
-#| xargs -L1 pytest
 
 pytest tests/
 
